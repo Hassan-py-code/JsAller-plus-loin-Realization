1e25bdc8bba2df1f524178179aed5c28737f8100
Hassan <havhassanb@gmail.com>
2025-11-24 20:38:13 +0100

    creat filter

--- main.js
+++ main.js
@@ -2,68 +2,51 @@
 
 let product_list=document.querySelector(".list_products");
 
+let all_products=[];
+
 
 fetch('https://dummyjson.com/products/category/smartphones')
 .then(response=>response.json())
 .then(data=>{
 
-   
-   data.products.forEach(element => {
+   all_products.push(...data.products);
+    products_func(all_products);
+       
 
-     let new_div=document.createElement("div");
-     new_div.classList.add("products");
-    
+}).catch(error=>{
+     console.log("You have" ,error);
+})
 
-   function getStars(rating) {
-     let stars = "";
-    for (let i = 1; i <= 5; i++) {
-        if (i <= rating) {
-            stars += `<i class="fa-regular fa-star" id="star"></i>`; 
-        }else{
-             stars+=`<i class="fa-solid fa-star-half-stroke" id="star"></i>`
-        }
-    }
-    return stars;
-}
+fetch('https://dummyjson.com/products/category/laptops')
+.then(response=>response.json())
+.then(data => {
 
-     function add_products(){
-           new_div.innerHTML=`
-         <p>${element.brand}</p>
-         <div class="images"><img src=${element.images[2]}></div>
-         <h2>${element.title}</h2>
-         <p>${element.description}</p>
-         <p> ${getStars(Math.round(element.rating))}</p>
-          <di class="shop_now"v>
-               <p>${element.price} $<p>
-         <span><i class="fa-solid fa-cart-plus"></i></span>
-          </div>
-        
-     `;
-     }
-
-
-       add_products();
-     product_list.appendChild(new_div);
-
-    });
-
+    all_products.push(...data.products);
+    products_func(all_products);
+ 
 
 }).catch(error=>{
      console.log("You have" ,error);
 })
 
 
+// create function
+function products_func(add_products){
 
-fetch('https://dummyjson.com/products/category/laptops')
-.then(response=>response.json())
-.then(data => {
-
-   data.products.forEach(element => {
+    product_list.innerHTML="";
+    add_products.forEach(element => {
+           let star="";
+           for (let index = 1; index <= 5; index++) {
+                if(index<Math.round(element.rating)) {
+                     star+=`<i class="fa-regular fa-star" id="star"></i>`;
+                }else{
+                     star+=`<i class="fa-solid fa-star-half-stroke" id="star"></i>`;
+                }
+           }
+        
      let new_div = document.createElement("div");
      new_div.classList.add("products");
-     
-           new_div.innerHTML=`
-         
+     new_div.innerHTML=`
          <p>${element.brand}</p>
          <div class="images"><img src=${element.images[2]}></div>
          <h2>${element.title}</h2>
@@ -71,14 +54,32 @@ fetch('https://dummyjson.com/products/category/laptops')
         <div class="shop_now">
                <p>${element.price} $<p>
          <span><i class="fa-solid fa-cart-plus"></i></span>
-          </div>
+        </div>
+        <p>${star}</p>
+      
      `;
-   
-
+      
      product_list.appendChild(new_div);
 
     });
+}
 
-}).catch(error=>{
-     console.log("You have" ,error);
-})
+
+document.querySelectorAll(".name_smartphone a").forEach(link => {
+       link.addEventListener("click",(event)=>{
+        event.preventDefault();
+
+     let brand=event.target.id;
+
+     let filter_brand = all_products.filter( p => p.brand.toLowerCase() === brand.toLowerCase());
+
+     products_func(filter_brand);
+
+        if (brand === "all") {
+            products_func(all_products);
+            return; 
+        };
+
+});
+
+});
